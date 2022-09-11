aa85005cda52c2c0f15680604baec02130209bfd
iByteABit256 <paulsmith4561@gmail.com>
2022-09-11 06:26:14 -0700

    Removed everything except solutions

---
+++ exercises/clippy/target/debug/deps/clippy2-fa96790a54c2722e.d
@@ -0,0 +1,7 @@
+/home/pauls/rustlings/exercises/clippy/target/debug/deps/clippy2-fa96790a54c2722e.rmeta: clippy2.rs
+
+/home/pauls/rustlings/exercises/clippy/target/debug/deps/clippy2-fa96790a54c2722e.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
